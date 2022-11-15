e4726e4e1b5469963a2283d5fa4baf3ef6407e5d
Kat Chudy <kec21@fsu.edu>
2022-11-15 17:01:57 -0500

    load image

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
+let img;
+function preload(){
+  img = loadImage('invisiblewoundgps.jpg');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  image(img,0,0)
 }
 
 function draw() {
