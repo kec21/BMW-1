dec9fcd155d94a6640f2aa742121bf7a1baf7a31
Kat Chudy <kec21@fsu.edu>
2022-11-16 13:54:32 -0500

    poly collide attempt

--- sketch.js
+++ sketch.js
@@ -5,10 +5,24 @@ img = loadImage('invisiblewound.jpg');
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  poly[0] = createVector(10,20);
+  poly[1] = createVector(30,40);
+  poly[2] = createVector(50,60);
+  //poly[3] = createVector();
+  //poly[4] = createVector();
+  //poly[5] = createVector();
+  //poly[6] = createVector();
+  //poly[7] = createVector();
 }
 
 function draw() {
   background(125);
   image(img, 0, 0, windowWidth, windowHeight);
-  
+  beginShape();
+  for (const {x,y} of poly) vertex(x,y);
+  endShape(CLOSE);
+  circle(mouseX,mouseY,10);
+  hit = collidePointPoly(mouseX,mouseY,poly);
+  stroke(hit ? color('red') : 0);
+  print('colliding?')
 }
