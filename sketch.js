91aee23dc47c3bff2df93e97f878133b1f07d3dc
Kat Chudy <kec21@fsu.edu>
2022-11-21 13:04:16 -0500

    add in Cobie's code

--- sketch.js
+++ sketch.js
@@ -5,8 +5,16 @@ function preload(){
 img = loadImage('invisiblewound.jpg')
 }
 
+var cnv;
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
 function setup() {
-  createCanvas(1000,1000);
+  cnv = createCanvas(1000,1000);
+  centerCanvas ();
   poly[0] = createVector(10,10);
   poly[1] = createVector(20,20);
   poly[2] = createVector(100,100);
@@ -15,6 +23,10 @@ function setup() {
   //poly[5] = createVector();
   //poly[6] = createVector();
   //poly[7] = createVector();
+}
+
+function windowResized() {
+  centerCanvas();
 }
 
 function draw() {
