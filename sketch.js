426c080cc3a98caf71a5554ae20f0aa5aab9f820
Kat Chudy <kec21@fsu.edu>
2022-11-26 17:31:00 -0500

    final complex poly coordinates

--- sketch.js
+++ sketch.js
@@ -23,44 +23,44 @@ function centerCanvas() {
 function setup() {
   cnv = createCanvas(1000,1000);
   centerCanvas ();
-  poly[0] = createVector(182,284);
-  poly[1] = createVector(251,287);
-  poly[2] = createVector(245,359);
-  poly[3] = createVector(299,383);
-  poly[4] = createVector(366,625);
-  poly[5] = createVector(289,635);
-  poly[6] = createVector(292,874);
-  poly[7] = createVector(183,878);
-  poly[8] = createVector(156,635);
-  poly[9] = createVector(85,620);
-  poly[10] = createVector(136,394);
-  poly[11] = createVector(186,365);
-  poly[12] = createVector();
-  poly[13] = createVector();
-  poly[14] = createVector();
-  poly[15] = createVector();
-  poly[16] = createVector();
-  poly[17] = createVector();
-  poly[18] = createVector();
-  poly[19] = createVector();
-  poly[20] = createVector();
-  poly[21] = createVector();
-  poly[22] = createVector();
-  poly[23] = createVector();
-  poly[24] = createVector();
-  poly[25] = createVector();
-  poly[26] = createVector();
-  poly[27] = createVector();
-  poly[28] = createVector();
-  poly[29] = createVector();
-  poly[30] = createVector();
-  poly[31] = createVector();
-  poly[32] = createVector();
-  poly[33] = createVector();
-  poly[34] = createVector();
-  poly[35] = createVector();
-  poly[36] = createVector();
-  poly[37] = createVector();
+  poly[0] = createVector(137,385);
+  poly[1] = createVector(180,362);
+  poly[2] = createVector(176,286);
+  poly[3] = createVector(253,285);
+  poly[4] = createVector(254,359);
+  poly[5] = createVector(306,386);
+  poly[6] = createVector(359,556);
+  poly[7] = createVector(367,467);
+  poly[8] = createVector(413,360);
+  poly[9] = createVector(398,319);
+  poly[10] = createVector(414,279);
+  poly[11] = createVector(593,279);
+  poly[12] = createVector(595,356);
+  poly[13] = createVector(642,463);
+  poly[14] = createVector(655,570);
+  poly[15] = createVector(696,394);
+  poly[16] = createVector(746,358);
+  poly[17] = createVector(741,282);
+  poly[18] = createVector(825,283);
+  poly[19] = createVector(820,361);
+  poly[20] = createVector(874,392);
+  poly[21] = createVector(904,562);
+  poly[22] = createVector(937,596);
+  poly[23] = createVector(913,648);
+  poly[24] = createVector(854,646);
+  poly[25] = createVector(842,886);
+  poly[26] = createVector(735,887);
+  poly[27] = createVector(715,649);
+  poly[28] = createVector(617,647);
+  poly[29] = createVector(624,850);
+  poly[30] = createVector(414,846);
+  poly[31] = createVector(398,644);
+  poly[32] = createVector(304,643);
+  poly[33] = createVector(286,889);
+  poly[34] = createVector(178,881);
+  poly[35] = createVector(150,650);
+  poly[36] = createVector(80,632);
+  poly[37] = createVector(81,582);
 }
 
 function windowResized() {
@@ -70,12 +70,12 @@ function windowResized() {
 function draw() {
   background(125);
   image(img, 0, 0, width, height);
-  beginShape();
-  for (const {x,y} of poly) vertex(x,y);
-  endShape(CLOSE);
-  circle(mouseX,mouseY,10);
-  stroke(hit ? color('red') : 0);
-  print('colliding?')
+  //beginShape();
+  //for (const {x,y} of poly) vertex(x,y);
+  //endShape(CLOSE);
+  //circle(mouseX,mouseY,10);
+  //stroke(hit ? color('red') : 0);
+  //print('colliding?')
 }
 
 function mouseClicked() {
